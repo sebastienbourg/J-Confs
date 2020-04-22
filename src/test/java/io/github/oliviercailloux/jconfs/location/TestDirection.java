f7b18252c13f47af84fe8f05564f12c93097c91e
anis468 <hamounianis@gmail.com>
2020-04-22 23:03:58 +0200

    correct identation

--- src/test/java/io/github/oliviercailloux/jconfs/location/TestDirection.java
+++ src/test/java/io/github/oliviercailloux/jconfs/location/TestDirection.java
@@ -9,12 +9,12 @@ class TestDirection {
 
 	@Test
 	void test() throws ApiException {
-		//fail("Not yet implemented");
-		 Direction d = Direction.given("2.287592,48.862725","2.3488,48.85341");// 13 Rue Cloche Percé, 75004 Paris et cadéro et du 11 Novembre, 75016 Paris  	
-			d.getDirection();		   		
-    	 System.out.println(d.getDistance()+" metres et "+d.getDuree()+ " seconds ");  
-    	 System.out.println(d.getSteps());
-	    
 
-    }
+		Direction d = Direction.given("2.287592,48.862725","2.3488,48.85341");// 13 Rue Cloche Percé, 75004 Paris et cadéro et du 11 Novembre, 75016 Paris  	
+		d.getDirection();		   		
+		System.out.println(d.getDistance()+" metres et "+d.getDuree()+ " seconds ");  
+		System.out.println(d.getSteps());
+
+
+	}
 }
