2feb3af6602889c62f15611de153a7f4302509e7
sebastienbourg <sebastien.bourg@dauphine.eu>
2020-05-21 10:46:31 +0200

    add location class

---
+++ src/test/java/io/github/oliviercailloux/jconfs/location/DistanceDurationTests.java
@@ -0,0 +1,35 @@
+package io.github.oliviercailloux.jconfs.location;
+
+import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.locationiq.client.ApiException;
+
+class DistanceDurationTests {
+
+	/**
+	 * 
+	 * Unit test in order to verify the good get of distance and duration. Carreful
+	 * this test depend on API calculation so it might fail in the future
+	 * 
+	 * @throws ApiException
+	 * @throws InterruptedException
+	 */
+
+	@Test
+	void testGetDistanceDuration() throws ApiException, InterruptedException {
+		BigDecimal distance = new BigDecimal("11168.5");
+		BigDecimal duration = new BigDecimal("1048.9");
+		DistanceDuration result = DistanceDuration.newDistanceDuration("13 Rue Cloche Percé, 75004 Paris",
+				"15 Avenue du général de gaulle, 92800 puteaux");
+		TimeUnit.SECONDS.sleep(1);
+		result.getDirection();
+		assertEquals(distance,result.getDistance());
+		assertEquals(duration,result.getDuration());
+
+	}
+}
---
+++ src/test/java/io/github/oliviercailloux/jconfs/location/TranslationAddressTest.java
@@ -0,0 +1,83 @@
+package io.github.oliviercailloux.jconfs.location;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+
+import com.locationiq.client.ApiException;
+
+/**
+ * 
+ * @author Floryan
+ *
+ */
+
+class TranslationAddressTest {
+
+	/**
+	 * Test of the creation of an newInstance of TranslationAddress
+	 */
+
+	@Test
+	public final void testCreateInstance() {
+		TranslationAddress t = TranslationAddress.newInstance();
+		assertEquals(null, t.getLatitude());
+		assertEquals(0, t.getAddressInformations().size());
+	}
+
+	/**
+	 * This method test the correct recovery informations returned by the
+	 * autocomplete method of LocationIQ
+	 * 
+	 * @throws LocationIq.ApiException
+	 * @throws InterruptedException
+	 */
+
+	@Test
+	public final void testRecoveryAddressInformations() throws ApiException, InterruptedException {
+		TranslationAddress t = TranslationAddress.newInstance();
+		TimeUnit.SECONDS.sleep(1);
+		t.recoveryAddressInformations("Université paris dauphine");
+		boolean test = (t.getAddressInformations().size() > 2);
+		assertEquals(true, test);
+	}
+
+	/**
+	 * This method tests the recovery of several addresses informations associated
+	 * with a search. In this case we know that Université paris dauphine might
+	 * return more than 1 address.
+	 * 
+	 * @throws LocationIq.ApiException
+	 * @throws InterruptedException
+	 */
+
+	@Test
+	public final void testRecoveryAddressFound() throws ApiException, InterruptedException {
+		TranslationAddress t = TranslationAddress.newInstance();
+		t.recoveryAddressInformations("Université paris dauphine");
+		TimeUnit.SECONDS.sleep(1);
+		t.recoveryAddressFound();
+		boolean test = (t.getAddressFound().size() > 2);
+		assertEquals(true, test);
+	}
+
+	/**
+	 * This method tests the builder of TranslationAddress
+	 * 
+	 * @throws LocationIq.ApiException
+	 */
+
+	@Test
+	public final void testBuilder() throws ApiException {
+		TranslationAddress address = TranslationAddress.TranslationAddressBuilder.build()
+				.addressInformations("Avenue jean rostand domont 95330").addressFound().latitude().longitude().get();
+		assertTrue(address.getAddressFound().contains("Avenue Jean Rostand, Domont, Ile-de-France, 95330, France"));
+		assertFalse(address.getLatitude().isEmpty());
+		assertFalse(address.getLongitude().isEmpty());
+	}
+
+}
